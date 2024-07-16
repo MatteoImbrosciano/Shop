e10e38a61473ad9df277d5d1a09619936f39f7e6
MatteoImbrosciano <82762466+MatteoImbrosciano@users.noreply.github.com>
2024-07-16 10:59:27 +0200

    Delete product-shop-session-main directory

--- product-shop-session-main/product-shop-session/src/main/java/shop/productshopsession/data/Acquisto.java
+++
@@ -1,6 +0,0 @@
-package shop.productshopsession.data;
-
-import java.io.Serializable;
-
-public record Acquisto(String nome,  double prezzo) implements Serializable {
-}
--- product-shop-session-main/product-shop-session/src/main/java/shop/productshopsession/data/Status.java
+++
@@ -1,8 +0,0 @@
-package shop.productshopsession.data;
-
-import java.io.Serializable;
-import java.time.Instant;
-import java.util.List;
-
-public record Status(List<Acquisto> acquisti, Instant timestamp) implements Serializable {
-}
